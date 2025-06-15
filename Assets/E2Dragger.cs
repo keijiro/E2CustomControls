5d9f1f94751bd6520f51f96d3ced67097c342917
Keijiro Takahashi <keijiro@gmail.com>
2025-06-15 16:56:15 +0900

    initial commit

---
+++ Assets/E2Dragger.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace E2CustomControls {
+
+sealed class E2Dragger : PointerManipulator
+{
+    #region Private variables
+
+    E2Knob _knob;
+    int _pointerID;
+    (Vector3 origin, float value, bool ready) _start;
+
+    bool IsActive => _pointerID >= 0;
+
+    #endregion
+
+    #region PointerManipulator implementation
+
+    public E2Dragger(E2Knob knob)
+    {
+        (_knob, _pointerID) = (knob, -1);
+        activators.Add(new ManipulatorActivationFilter{button = MouseButton.LeftMouse});
+    }
+
+    protected override void RegisterCallbacksOnTarget()
+    {
+        target.RegisterCallback<PointerDownEvent>(OnPointerDown);
+        target.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+        target.RegisterCallback<PointerUpEvent>(OnPointerUp);
+    }
+
+    protected override void UnregisterCallbacksFromTarget()
+    {
+        target.UnregisterCallback<PointerDownEvent>(OnPointerDown);
+        target.UnregisterCallback<PointerMoveEvent>(OnPointerMove);
+        target.UnregisterCallback<PointerUpEvent>(OnPointerUp);
+    }
+
+    #endregion
+
+    #region Pointer callbacks
+
+    void OnPointerDown(PointerDownEvent e)
+    {
+        if (IsActive)
+        {
+            e.StopImmediatePropagation();
+        }
+        else if (CanStartManipulation(e))
+        {
+            _start = (e.localPosition, _knob.value, true);
+            target.CapturePointer(_pointerID = e.pointerId);
+            e.StopPropagation();
+        }
+    }
+
+    void OnPointerMove(PointerMoveEvent e)
+    {
+        if (!IsActive || !target.HasPointerCapture(_pointerID)) return;
+
+        if (!_start.ready) _start = (e.localPosition, _knob.value, true);
+
+        var diff = e.localPosition - _start.origin;
+        var delta = (diff.x - diff.y) / 100;
+        _knob.value = (int)(_start.value + delta * (_knob.highValue - _knob.lowValue));
+
+        e.StopPropagation();
+    }
+
+    void OnPointerUp(PointerUpEvent e)
+    {
+        if (!IsActive || !target.HasPointerCapture(_pointerID)) return;
+
+        if (CanStopManipulation(e))
+        {
+            _pointerID = -1;
+            target.ReleaseMouse();
+            e.StopPropagation();
+        }
+    }
+
+    #endregion
+}
+
+} // namespace E2CustomControls
---
+++ Assets/E2Knob.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace E2CustomControls {
+
+[UxmlElement]
+public partial class E2Knob : BaseField<int>
+{
+    #region Public UI properties
+
+    [UxmlAttribute]
+    public int lowValue { get => _lowValue; set => SetLowValue(value); }
+
+    [UxmlAttribute]
+    public int highValue { get => _highValue; set => SetHighValue(value); }
+
+    #endregion
+
+    #region Property backend
+
+    int _lowValue = 0;
+    int _highValue = 100;
+
+    void SetLowValue(int value)
+    {
+        _lowValue = value;
+        SetValueWithoutNotify(this.value);
+    }
+
+    void SetHighValue(int value)
+    {
+        _highValue = value;
+        SetValueWithoutNotify(this.value);
+    }
+
+    #endregion
+
+    #region USS class names
+
+    public static readonly new string ussClassName = "e2-knob";
+    public static readonly new string labelUssClassName = "e2-knob__label";
+
+    #endregion
+
+    #region Visual element implementation
+
+    E2KnobInput _input;
+
+    public E2Knob() : this(null) {}
+
+    public E2Knob(string label) : base(label, new E2KnobInput())
+    {
+        AddToClassList(ussClassName);
+        labelElement.AddToClassList(labelUssClassName);
+        _input = (E2KnobInput)this.Q(className: E2KnobInput.ussClassName);
+        _input.AddManipulator(new E2Dragger(this));
+    }
+
+    public override void SetValueWithoutNotify(int newValue)
+    {
+        newValue = Mathf.Clamp(newValue, lowValue, highValue);
+        base.SetValueWithoutNotify(newValue);
+        _input.NormalizedValue = (float)(newValue - lowValue) / (highValue - lowValue);
+        _input.MarkDirtyRepaint();
+    }
+
+    #endregion
+}
+
+} // namespace E2CustomControls
