1d972df90ca52f6c228280b870f40e5cb8cc83a9
Keijiro Takahashi <keijiro@gmail.com>
2025-06-16 21:53:14 +0900

    stylesheet

--- Assets/E2Keyboard/PianoKey.cs
+++ Assets/E2Keyboard/PianoKey.cs
@@ -30,43 +30,10 @@ public class PianoKey : VisualElement
         AddToClassList(isBlackKey ? "piano-key--black" : "piano-key--white");
         
         this.RegisterCallback<ClickEvent>(OnClick);
-        
-        SetupStyle();
-    }
-
-    void SetupStyle()
-    {
-        if (isBlackKey)
-        {
-            style.backgroundColor = new Color(0.2f, 0.2f, 0.2f);
-            style.borderTopWidth = 1;
-            style.borderBottomWidth = 1;
-            style.borderLeftWidth = 1;
-            style.borderRightWidth = 1;
-            style.borderTopColor = Color.black;
-            style.borderBottomColor = Color.black;
-            style.borderLeftColor = Color.black;
-            style.borderRightColor = Color.black;
-        }
-        else
-        {
-            style.backgroundColor = Color.white;
-            style.borderTopWidth = 1;
-            style.borderBottomWidth = 1;
-            style.borderLeftWidth = 1;
-            style.borderRightWidth = 1;
-            style.borderTopColor = new Color(0.7f, 0.7f, 0.7f);
-            style.borderBottomColor = new Color(0.7f, 0.7f, 0.7f);
-            style.borderLeftColor = new Color(0.7f, 0.7f, 0.7f);
-            style.borderRightColor = new Color(0.7f, 0.7f, 0.7f);
-            
-            style.flexGrow = 1;
-            style.height = Length.Percent(100);
-        }
-        
         this.RegisterCallback<MouseEnterEvent>(OnMouseEnter);
         this.RegisterCallback<MouseLeaveEvent>(OnMouseLeave);
     }
+
 
     void OnClick(ClickEvent evt)
     {
@@ -77,14 +44,7 @@ public class PianoKey : VisualElement
     {
         if (!isPressed)
         {
-            if (isBlackKey)
-            {
-                style.backgroundColor = new Color(0.3f, 0.3f, 0.3f);
-            }
-            else
-            {
-                style.backgroundColor = new Color(0.95f, 0.95f, 0.95f);
-            }
+            AddToClassList("piano-key--hover");
         }
     }
 
@@ -92,14 +52,7 @@ public class PianoKey : VisualElement
     {
         if (!isPressed)
         {
-            if (isBlackKey)
-            {
-                style.backgroundColor = new Color(0.2f, 0.2f, 0.2f);
-            }
-            else
-            {
-                style.backgroundColor = Color.white;
-            }
+            RemoveFromClassList("piano-key--hover");
         }
     }
 
@@ -112,26 +65,11 @@ public class PianoKey : VisualElement
         if (pressed)
         {
             AddToClassList("piano-key--pressed");
-            if (isBlackKey)
-            {
-                style.backgroundColor = new Color(0.4f, 0.6f, 1.0f); // 青系
-            }
-            else
-            {
-                style.backgroundColor = new Color(0.6f, 0.8f, 1.0f); // 明るい青系
-            }
+            RemoveFromClassList("piano-key--hover");
         }
         else
         {
             RemoveFromClassList("piano-key--pressed");
-            if (isBlackKey)
-            {
-                style.backgroundColor = new Color(0.2f, 0.2f, 0.2f);
-            }
-            else
-            {
-                style.backgroundColor = Color.white;
-            }
         }
     }
 
