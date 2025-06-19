79ad749dfc546cc802efd988bf8c58414b048e0b
Keijiro Takahashi <keijiro@gmail.com>
2025-06-19 23:06:33 +0900

    test code

--- Assets/Test/E2KeyboardTest.cs
+++ Assets/Test/E2KeyboardTest.cs
@@ -6,31 +6,22 @@ namespace E2Controls {
 
 public sealed class E2KeyboardTest : MonoBehaviour
 {
+    void Start()
+      => GetComponent<UIDocument>().rootVisualElement.Q<E2ChordKeyboard>()
+           .RegisterCallback<ChangeEvent<(int, int, int, int)>>(OnChordChanged);
+
     static readonly string[] NoteNames =
       { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
 
-    string GetNoteName(int note)
+    static string GetNoteName(int note)
       => $"{NoteNames[note % 12]}{note / 12 - 1}";
-
-    Label _label;
-
-    void Start()
-    {
-        var root = GetComponent<UIDocument>().rootVisualElement;
-
-        var chordKeyboard = new E2ChordKeyboard();
-        chordKeyboard.RegisterCallback<ChangeEvent<(int, int, int, int)>>(OnChordChanged);
-        root.Add(chordKeyboard);
-
-        _label = new Label();
-        root.Add(_label);
-    }
 
     void OnChordChanged(ChangeEvent<(int, int, int, int)> evt)
     {
         var (note1, note2, note3, note4) = evt.newValue;
         var notes = new[]{ note1, note2, note3, note4 }.Where(n => n >= 0);
-        _label.text = string.Join(", ", notes.Select(GetNoteName));
+        GetComponent<UIDocument>().rootVisualElement.Q<Label>("chord-label")
+          .text = string.Join(", ", notes.Select(GetNoteName));
     }
 }
 
